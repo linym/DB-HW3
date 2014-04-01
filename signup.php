c00489e167572d70f6ef3b2b58f46a608718bf24
linym <rexlin2002001@gmail.com>
2014-04-02 03:45:55 +0800

    create favorite table while signing up

--- signup.php
+++ signup.php
@@ -17,6 +17,28 @@ if($result->password){
 	$hash = crypt($_POST['password']);
 	$sth->execute(array($_POST["email"],$hash,$_POST["admin"]));
 
+	$sqll ="CREATE TABLE  `{$_POST['email']}` (
+`id` INT( 10 ) UNSIGNED NOT NULL ,
+`flight_number` VARCHAR( 255 ) NOT NULL ,
+`departure` VARCHAR( 255 ) NOT NULL ,
+`destination` VARCHAR( 255 ) NOT NULL ,
+`departure_date` DATETIME NOT NULL ,
+`arrival_date` DATETIME NOT NULL ,
+`price` DOUBLE NOT NULL ,
+PRIMARY KEY (  `id` ) ,
+INDEX (  `departure` ) ,
+INDEX (  `destination` )
+) ENGINE = INNODB;
+
+ALTER TABLE  `{$_POST['email']}` ADD FOREIGN KEY (  `departure` ) REFERENCES  `airport` (`location`) ON DELETE CASCADE ON UPDATE CASCADE ;
+ALTER TABLE  `{$_POST['email']}` ADD FOREIGN KEY (  `destination` ) REFERENCES  `airport` (`location`) ON DELETE CASCADE ON UPDATE CASCADE ;
+ALTER TABLE  `{$_POST['email']}` ADD FOREIGN KEY (  `id` ) REFERENCES  `flight` (`id`) ON DELETE CASCADE ON UPDATE CASCADE ;
+
+
+";
+	$db->exec($sqll);	
+
+	
 	header("Refresh: 3; url=signin.php");?>
 	<center><font size="10">sign up successfully, wait 3 seconds to return</font></center>
 <?php
